9a64a6414ce4b3fedb8686aca4c70f325eb8adf3
Squmon <squmon074@gmail.com>
2025-02-02 21:53:28 +0500

    second commit

--- sorter.py
+++
@@ -1,5 +0,0 @@
-with open("main.py", 'r') as j:
-    content = map(lambda x: '#' + x, j.read().split("#"))
-for n, c in enumerate(content):
-    with open(f"functions1/task{n}.py", 'w') as j:
-        j.write(c)
